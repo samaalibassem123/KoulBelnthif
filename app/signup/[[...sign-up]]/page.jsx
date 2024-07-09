58cba7834ef332749add59870f7b2395cbb14e64
bassem samaali <83089177+samaalibassem123@users.noreply.github.com>
2024-07-09 19:58:44 +0100

    Delete app/signup/[[...sign-up]] directory

--- app/signup/[[...sign-up]]/page.jsx
+++
@@ -1,9 +0,0 @@
-import { SignUp } from "@clerk/nextjs";
-
-export default function Page() {
-  return (
-    <main className="flex w-full p-2 justify-center items-center">
-      <SignUp />
-    </main>
-  );
-}
