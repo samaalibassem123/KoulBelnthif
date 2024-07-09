67041c8a4e0bad96ff54b45b5fe89aa3028e9063
bassem samaali <83089177+samaalibassem123@users.noreply.github.com>
2024-07-09 19:58:34 +0100

    Delete app/sigin/[[...sign-in]] directory

--- app/sigin/[[...sign-in]]/page.jsx
+++
@@ -1,9 +0,0 @@
-import { SignIn } from "@clerk/nextjs";
-
-export default function Page() {
-  return (
-    <main className="flex p-2  w-full h-screen  justify-center items-center">
-      <SignIn />
-    </main>
-  );
-}
