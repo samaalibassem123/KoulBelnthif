814783dc73c5c15237f420c5dbd80930b65d925a
bassem samaali <83089177+samaalibassem123@users.noreply.github.com>
2024-07-10 00:03:09 +0100

    Update page.jsx

--- app/user/[id]/page.jsx
+++ app/user/[id]/page.jsx
@@ -1,6 +1,14 @@
 import Header from "@/components/Header";
+import Dashboard from "@/components/UserAccount/Dashboard";
+import FormInfo from "@/components/UserAccount/FormInfo";
 
 export default function page({ params }) {
   const id = params.id;
-  return <Header />;
+  const HasSubmit = true;
+  return (
+    <main className="flex flex-col items-center">
+      <Header />
+      {HasSubmit ? <Dashboard /> : <FormInfo />}
+    </main>
+  );
 }
