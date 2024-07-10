28a826557869a8f8a3227f144cd212dca1128eb6
bassem samaali <83089177+samaalibassem123@users.noreply.github.com>
2024-07-10 23:59:43 +0100

    Update page.jsx

--- app/user/[id]/page.jsx
+++ app/user/[id]/page.jsx
@@ -1,14 +1,17 @@
 import Header from "@/components/Header";
-import Dashboard from "@/components/UserAccount/Dashboard";
-import FormInfo from "@/components/UserAccount/FormInfo";
+import Dashboard from "@/components/UserAccount/Dashboard/Dashboard";
+import FormInfo from "@/components/UserAccount/Formulaire/FormInfo";
+import FormFood from "@/components/UserAccount/Formulaire/FormFood";
 
 export default function page({ params }) {
   const id = params.id;
-  const HasSubmit = true;
+
   return (
     <main className="flex flex-col items-center">
       <Header />
-      {HasSubmit ? <Dashboard /> : <FormInfo />}
+      <Dashboard />
+      <FormInfo />
+      <FormFood />
     </main>
   );
 }
